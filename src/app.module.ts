6838a831308ef7af22b99c25545a00e3f974df06
lu_maraes <50714793+Lumaraescoder@users.noreply.github.com>
2023-10-11 17:05:30 +0100

    Setup project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import { ProductsModule } from './products/products.module';
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://lumaraess:biwPKBf9oJQbHmT0@cluster0.qrvekqy.mongodb.net/',
+    ),
+    ProductsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
