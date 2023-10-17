1a26a4f0426184b700b7311ddd89fe3bc14171fb
lu_maraes <50714793+Lumaraescoder@users.noreply.github.com>
2023-10-17 13:04:40 +0100

    correcting spelling names

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { StackholdersModule } from './stackholder/stackeholder.module';
+import { StakeholdersModule } from './stackholder/stackeholder.module';
 @Module({
   imports: [
     MongooseModule.forRoot(
       'mongodb+srv://lumaraess:biwPKBf9oJQbHmT0@cluster0.qrvekqy.mongodb.net/',
     ),
-    StackholdersModule,
+    StakeholdersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
