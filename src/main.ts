add4034fccf8a3427e2c1da0f0ffb1a9831c9776
lu_maraes <50714793+Lumaraescoder@users.noreply.github.com>
2023-10-19 02:08:13 +0100

    up

--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config } from 'dotenv';
+import * as cors from 'cors';
+
 config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'https://galp.vercel.app',
-      'http://galp.vercel.app',
-    ],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-    allowedHeaders: 'Content-Type, Accept',
-  });
+  app.use(
+    cors({
+      origin: [
+        'http://localhost:3000',
+        'https://galp.vercel.app',
+        'http://galp.vercel.app',
+      ],
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+      credentials: true,
+      allowedHeaders: 'Content-Type, Accept',
+    }),
+  );
 
   await app.listen(3333);
 }
